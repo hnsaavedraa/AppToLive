6f9f14f89d4fd8ab3f25ad500e13cfa9dea73388
Fernando Morantes <josefernandomorantes@gmail.com>
2019-03-03 21:24:17 -0500

    Creating the app skeleton

---
+++ js/map.js
@@ -0,0 +1,11 @@
+var map;
+
+function onGoogleMapResponse() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {
+      lat: 4.6990,
+      lng: -74.1615
+    }
+  });
+}
